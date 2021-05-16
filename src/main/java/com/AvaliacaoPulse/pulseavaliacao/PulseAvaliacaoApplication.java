62b8a6ffdbbf0b942b9abbcfa498e26dd5132f69
Lucas Cantanhêde <lucas.cantanhede>
2021-05-16 15:01:43 -0300

    iniciando commits da aplicação

---
+++ src/main/java/com/AvaliacaoPulse/pulseavaliacao/PulseAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.AvaliacaoPulse.pulseavaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AvaliacaoPulse/pulseavaliacao/PulseAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AvaliacaoPulse.pulseavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
